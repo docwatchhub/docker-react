ae9dce9564f0d5273223bdf28669c743d2486091
simon <docwatch@protonmail.com>
2022-04-03 03:46:45 -0500

    changed app message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by Simon
+          I was changed on the develop branch!
         </a>
       </header>
     </div>
